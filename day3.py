c6db5df8c5b6af914e8081d5a88fc24348fc0bc2
Jonás Casteglione <jnss.cas@gmail.com>
2021-12-14 00:23:21 -0300

    day 3 part 2

--- day3.py
+++ day3.py
@@ -47,5 +47,65 @@ def part_1():
         return gamma_rate_decimal * epsilon_rate_decimal  # power consumption
 
 
+def get_filter_by_rating_type(rating_type):
+    if rating_type == 'oxygen':
+        return '1'
+    return '0'
+
+
+def get_rating_by_type(codes, rating_type):
+    code_length = len(codes[0])
+    bit_start_position = 0
+    while bit_start_position < code_length:
+        bit_position = bit_start_position
+        codes_filtered = codes
+        while bit_position < code_length and len(codes_filtered) > 0:
+            sum_result = count_ones_and_zeros(codes_filtered, bit_position)
+            bit_to_filter = get_bit_to_filter(sum_result, rating_type)
+            codes_filtered = list(filter(lambda code_record: code_record[bit_position] == bit_to_filter, codes_filtered))
+            if len(codes_filtered) == 1:
+                return codes_filtered[0]
+            bit_position += 1
+        bit_start_position += 1
+    return None
+
+
+def get_bit_to_filter(sum_result, rating_type):
+    if rating_type == 'oxygen':
+        if sum_result['ones'] >= sum_result['zeros']:
+            return '1'
+        return '0'
+    else:
+        if sum_result['ones'] >= sum_result['zeros']:
+            return '0'
+        return '1'
+
+
+def count_ones_and_zeros(codes, bit_position):
+    sum_ones_and_zeros = {'zeros': 0, 'ones': 0}
+    for code in codes:
+        bit = code[bit_position]
+        if bit == '0':
+            sum_ones_and_zeros['zeros'] += 1
+        else:
+            sum_ones_and_zeros['ones'] += 1
+    return sum_ones_and_zeros
+
+
+def part_2():
+    with open('input_day3.txt') as f:
+
+        codes = []
+        for line in f:
+            code = str(line).rstrip()
+            codes.append(code)
+
+        oxygen_generator_rating = get_rating_by_type(codes, 'oxygen')
+        co2_scrubber_rating = get_rating_by_type(codes, 'co2')
+
+        return int(oxygen_generator_rating, 2) * int(co2_scrubber_rating, 2)
+
+
 if __name__ == '__main__':
     print(part_1())
+    print(part_2())
